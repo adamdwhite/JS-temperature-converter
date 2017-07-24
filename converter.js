9137d16e9f4dcbc92ce64207f096a6c001ea0739
Adam D. White <adamwhite82@gmail.com>
2017-07-24 18:46:07 -0500

    finished all steps of the exercise, added the color indicator conditionals for output.

--- converter.js
+++ converter.js
@@ -2,50 +2,48 @@ console.log("let's convert these temperatures!")
 
 //first define all the buttons with get elements by ID:
 
-let celsiusRadio = document.getElementById("celsius");
-let farenheitRadio = document.getElementById("farenheit");
-let convertButton = document.getElementById("convert");
-let clearButton = document.getElementById("clear");
-let inputField = document.getElementById("enteredTemp");
-let outputField = document.getElementById("output");
+var celsiusRadio = document.getElementById("celsius");
+var farenheitRadio = document.getElementById("farenheit");
+var convertButton = document.getElementById("convert");
+var clearButton = document.getElementById("clear");
+var inputField = document.getElementById("enteredTemp");
+var outputField = document.getElementById("output");
 
 
-// function checkInput() {
-// //if the 
-//    if (convertButton.addEventListener("checked", tempMath();
-// //and   
-//     inputField.value = ();
-// //get the value of the inputField  
-// //console log the input value 
-// }
+//now create a function that will do the conversion math when called. 
 
 function tempMath() {
     if (celsiusRadio.checked) {
-        outputField = ((inputField - 32) * (5 / 9)).toFixed(2);
-        if (farenheitRadio.checked) {
-            outputField = ((inputField * 9 / 5) + 32);
-        }
-    };
-
-
-    // and a function which will highlight the output field in RED if the number is > "#",
-    // and will highlight the output field in BLUE if the number is < "#".
-
-    // function outputTooMuch() {
-    //     if (outputField.value.length < 0) {
-    //         //make the text DARK BLUE  
-    //     }
-    //     else if (outputField.value.length > 90) {
-    //         //make the text DARK RED
-    //     }
-    // };
-
-    //create a function which will clear the output when the "CLEAR OUTPUT" button is clicked 
-    function resetOutput() {
-        document.getElementById("enteredTemp").value = 0;
-        outputField.innerHTML = " ";
+        console.log(inputField.value, "Celsius");
+        inputField.value = parseFloat(inputField.value);
+        outputField.value = ((inputField.value - 32) * (5 / 9)).toFixed(2);
+    } else {
+        (farenheitRadio.checked);
+        console.log(inputField.value, "Farenheit");
+        inputField.value = parseFloat(inputField.value);
+        outputField.value = ((inputField.value * 9 / 5) + 32).toFixed(2);
     }
+};
+convertButton.addEventListener("click", tempMath);
+
+// and a function which will highlight the output field in RED if the number is > "#",
+// and will highlight the output field in BLUE if the number is < "#".
+
+function changeColor() {
+    if (outputField.value < 0) {
+        document.getElementById("output").style.color = 'blue';
+    } else if (outputField.value > 90) {
+        document.getElementById("output").style.color = 'red';
+    } else document.getElementById("output").style.color = 'green';
+};
+convertButton.addEventListener("click", changeColor);
+
+//create a function which will clear the output when the "CLEAR OUTPUT" button is clicked 
+function resetOutput() {
+    document.getElementById("enteredTemp").value = " ";
+    document.getElementById("output").value = " ";
 }
+clearButton.addEventListener("click", resetOutput);
 
 // //Original Exercise Notes Below:
 
